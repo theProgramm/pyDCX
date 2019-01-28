8ac9304208c8655ea5fc45b1b4e5445a4c1904f8
Patrick Friedel <hut@hadiko.de>
2019-01-29 00:05:17 +0100

    added static files

---
+++ static/duinoDCX_frontend/precache-manifest.b483ace48fec04ba7ffd5b29869a86c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+    {
+        "revision": "c2b974a3363562b5ca81",
+        "url": "/static/css/main.e4cafdf7.chunk.css"
+    },
+    {
+        "revision": "c2b974a3363562b5ca81",
+        "url": "/static/js/main.c2b974a3.chunk.js"
+    },
+    {
+        "revision": "d8c90f4b371bf4a9ffb2",
+        "url": "/static/css/1.9972ba67.chunk.css"
+    },
+    {
+        "revision": "d8c90f4b371bf4a9ffb2",
+        "url": "/static/js/1.d8c90f4b.chunk.js"
+    },
+    {
+        "revision": "229c360febb4351a89df",
+        "url": "/static/js/runtime~main.229c360f.js"
+    },
+    {
+        "revision": "448c34a56d699c29117adc64c43affeb",
+        "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+    },
+    {
+        "revision": "f4769f9bdb7466be65088239c12046d1",
+        "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+    },
+    {
+        "revision": "fa2772327f55d8198301fdb8bcfc8158",
+        "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+    },
+    {
+        "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+        "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+    },
+    {
+        "revision": "89889688147bd7575d6327160d64e760",
+        "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+    },
+    {
+        "revision": "607857bbf1a84e05883a6e15636b90de",
+        "url": "/index.html"
+    }
+];
