fb6ffe4d0f631bf39e953c9585710ff6712546ca
Patrick Friedel <hut@hadiko.de>
2019-01-31 13:53:06 +0100

    added typehints for protocoll

--- protocoll.py
+++ protocoll.py
@@ -1,5 +1,6 @@
 import math
 from dataclasses import dataclass
+from typing import List
 
 import const
 
@@ -42,74 +43,77 @@ class DirectCommandParameter:
         else:
             raise ValueError("need either integer value or direct bytes")
 
-    def as_bytes(self):
+    def as_bytes(self) -> bytes:
         return self.channel.to_bytes(1, "big") + self.param_num.to_bytes(1, "big") + self.params
 
 
 @dataclass
 class DirectCommand:
-    params: list[DirectCommandParameter]
+    params: List[DirectCommandParameter]
 
     def __init__(self):
         self.params = []
 
-    def add_param(self, func: int, value: int = None, direct_params: bytes = None):
+    def add_new_param(self, func: int, value: int = None, direct_params: bytes = None) -> None:
         self.params.append(DirectCommandParameter(func, value=value, direct_params=direct_params))
 
-    def as_query(self, device_id: int):
-        return Query(device_id, 0x3F, int_to_byte(len(self.params)) + b''.join([x.as_bytes() for x in self.params]))
+    def add_param(self, param: DirectCommandParameter):
+        self.params.append(param)
 
-    def as_bytes(self, device_id: int):
+    def as_query(self, device_id: int) -> Query:
+        return Query(device_id, 0x20, int_to_byte(len(self.params)) + b''.join([x.as_bytes() for x in self.params]))
+
+    def as_bytes(self, device_id: int) -> bytes:
         return self.as_query(device_id).as_bytes()
 
 
-def search():
+def search() -> bytes:
     return b'\xF0\x00\x20\x32\x20\x0E\x40\xF7'
 
 
-def ping(device_id: int):
+def ping(device_id: int) -> bytes:
     return Query(device_id, 0x44).as_bytes()
 
 
-def dump(device_id: int, part: int):
-    return Query(device_id, 0x50, part.to_bytes(1, "big")).as_bytes()
+def dump(device_id: int, part: int) -> bytes:
+    return Query(device_id, 0x50, b'\x01' + part.to_bytes(2, "big")).as_bytes()
 
 
-def set_transmit_mode(device_id: int):
+def set_transmit_mode(device_id: int) -> bytes:
     return Query(device_id, 0x3F, b'\x0C\x00').as_bytes()
 
 
-def int_to_byte(i: int):
+def int_to_byte(i: int) -> bytes:
     return i.to_bytes(1, "big")
 
 
-def calc_value_bytes(value: int):
+def calc_value_bytes(value: int) -> bytes:
     return int(value / 128).to_bytes(1, "big") + int(value % 128).to_bytes(1, "big")
 
 
-def bool_to_bytes(b: bool) -> int:
+def bool_to_int(b: bool) -> int:
     return 0x01 if b else 0x00
 
 
-def internal_volume_from_display_value(v: float):
+def internal_volume_from_display_value(v: float) -> int:
     return math.floor(v * 10) + 150
 
 
-def internal_threshold_from_display_value(v: int):
+def internal_threshold_from_display_value(v: int) -> int:
     return math.floor(v * 10) + 240
 
 
-def set_muted_param(channel_id: int, muted: bool):
-    return DirectCommandParameter(channel_id, 0x03, bool_to_bytes(muted))
+def set_muted_param(channel_id: int, muted: bool) -> DirectCommandParameter:
+    return DirectCommandParameter(channel_id, 0x03, bool_to_int(muted))
 
 
-def set_volume_param(channel_id: int, volume: int):
+def set_volume_param(channel_id: int, volume: int) -> DirectCommandParameter:
     return DirectCommandParameter(channel_id, 0x02, internal_volume_from_display_value(volume))
 
 
-def set_limiter_on_param(channel_id: int, on: bool):
-    return DirectCommandParameter(channel_id, 0x46, bool_to_bytes(on))
+def set_limiter_on_param(channel_id: int, on: bool) -> DirectCommandParameter:
+    return DirectCommandParameter(channel_id, 0x46, bool_to_int(on))
 
 
-def set_limiter_threshold_param(channel_id: int, threshold: int):
+def set_limiter_threshold_param(channel_id: int, threshold: int) -> DirectCommandParameter:
     return DirectCommandParameter(channel_id, 0x47, internal_threshold_from_display_value(threshold))
